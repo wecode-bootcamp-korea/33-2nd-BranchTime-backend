d92ae5d2a9e41650363a7f05cd9eaf19c62d97c9
sorkihg <sorkihg@gmail.com>
2022-06-09 13:52:25 +0900

    [BranchTime팀] models.py 일부수정
    [수정내역]
    1. users/models.py 15번줄 social_acccunt_id > social_account_id 수정
    2. user/models.py 17번줄 social_login > user

--- users/models.py
+++ users/models.py
@@ -12,9 +12,9 @@ class User(TimeStampedModel):
         db_table = 'users'
 
 class SocialAccount(TimeStampedModel):
-    social_acccunt_id = models.CharField(max_length=300)
+    social_account_id = models.CharField(max_length=300)
     name              = models.CharField(max_length=50)
-    social_login      = models.ForeignKey(User, on_delete=models.CASCADE)
+    uesr              = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'social_accounts'
