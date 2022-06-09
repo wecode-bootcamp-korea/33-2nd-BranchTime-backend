faa47db3396dbae3b391cd638178b3aec2f79617
sorkihg <sorkihg@gmail.com>
2022-06-09 12:59:42 +0900

    [BranchTime팀] KakaoLoginView 구현 및 피드백 반영
    [피드백 반영 내용]
    1. 서버응답 성공/실패 여부 판단 status_code로 비교하는 기능 추가
    2. 1.과 관련하여 test.py status_code 테스트 코드 추가
    3. transaction 사용하여 동시저장안될시 롤백 기능 추가
    4. 46번줄 user.id 관련 38번줄 반환하는 user 활용하는 기능 추가
    5. requests모듈 사용시 응답관련 timeout 기능 추가
    
    [기타사항]
    1. 소셜로그인 인증과정 삭제 (프론트엔드에서 기능구현예정)
    2. 1. 로 인하여 client_id와 redirect_uri 프론트엔드에서 받아서 수정완료
    3. 46번줄 introduction 추가 기본값 설정
    4. models.py 오탈자로 인해 51번줄 변수명 수정 uesr_id > user_id

--- users/views.py
+++ users/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+import jwt
+import requests
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+from django.shortcuts import redirect
+from django.conf import settings
+from django.db import transaction
+
+from users.models import SocialAccount, User
+
+class KakaoLoginView(View):
+    def get(self, request):
+        try:
+            code = request.GET.get("code")
+            kakao_token_api = "https://kauth.kakao.com/oauth/token"
+            data = {
+                "grant_type"  : "authorization_code",
+                "client_id"   : settings.CLIENT_ID,
+                "redirect_uri": "http://localhost:3000/oauth/callback/kakao",
+                "code"        : code
+            }
+            access_token  = requests.post(kakao_token_api, data=data, timeout=1).json()["access_token"]
+            kakao_api_url = "https://kapi.kakao.com/v2/user/me"
+
+            response = requests.get(kakao_api_url, headers={"Authorization": f"Bearer {access_token}"}, timeout=1)
+            if response.status_code == 401:
+                return JsonResponse({"Error":"token_error"}, status = 401)
+
+            user_info = response.json()
+            sociaaccount = SocialAccount.objects.filter(social_account_id  = user_info["id"])
+            if sociaaccount.exists():
+                access_token = jwt.encode({'id': sociaaccount.get().id}, settings.SECRET_KEY, settings.ALGORITHM)
+                return JsonResponse({"token":access_token}, status = 200)
+            
+            
+            with transaction.atomic():
+                user=User.objects.create(
+                        email        = user_info["kakao_account"]["email"],
+                        name         = user_info["properties"]["nickname"],
+                        thumbnail    = user_info["properties"]["profile_image"],
+                        introduction = user_info["properties"]["nickname"]+"님의 BranchTime입니다"
+                    )
+                SocialAccount.objects.create(
+                        social_account_id = user_info["id"],
+                        name              = "kakao",
+                        user_id           = user.id
+                        )
+                access_token = jwt.encode({'id':user.id}, settings.SECRET_KEY, settings.ALGORITHM)  
+                return JsonResponse({"token":access_token}, status = 200)
+        except KeyError:
+            return JsonResponse({"message":"KEY ERROR"}, status = 400)    
