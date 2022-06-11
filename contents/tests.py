ce1fa44b6d05aaed5b679deda894eb8437e39757
minjeong kim <ebsgreat7@gmail.com>
2022-06-11 13:32:50 +0900

    [FEAT] 글쓰기 내 이미지 업로드 기능 추가
    1. core앱
    -파일 업로드 기능 core앱에 추가 -> 추후 다른 기능에서도 이미지/영상 업로드 기능 추가를 위해 독립적으로 빼놓음
    -버킷 내 독립적인 폴더로 글쓰기 내 이미지 관리하기 위해 utils.py 추가 -> object_name 구성할때 글쓴 저자 id도 사용
    
    2. my_settings.py
    -AWS S3 관련 기능 세팅 완료
    
    3. content앱
    -content imageupload view에는 png, jpg, gif, jpeg 확장자 말고는 들어 올 수 없게 예외처리 -> 이미지 업로드 공간이기 때문에 위와 같이 처리, 영상을 업로드 하는 공간을 따로 있음(추가기능)
    
    - key는 content_image + user.id + content_image 확장자로 구성-> 해당 key와 media_url(aws 버킷 네임과 리전 주소를 포함한 media_ulr)로 만든 object_name을 그대로프론트에 보내줄 예정

--- contents/tests.py
+++ contents/tests.py
@@ -1,3 +1,63 @@
-from django.test import TestCase
+import json
+import bcrypt
+import jwt
+from io import BytesIO
 
-# Create your tests here.
+from django.test       import TestCase, Client
+from django.conf       import settings
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from .models           import User
+from unittest          import mock
+from unittest.mock     import patch, MagicMock
+from core.views        import upload_fileobj
+from my_settings       import MEDIA_URL
+
+class CommentImageUploadTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id           = 1,
+            email        = "dno06103@naver.com",
+            name         = "김민정",
+            thumbnail    = "저거.png",
+            introduction = "블라블라블라블라",
+        )
+
+        self.token = jwt.encode({'id':User.objects.get(id=1).id}, settings.SECRET_KEY, settings.ALGORITHM)
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    @patch("core.views.upload_fileobj")
+    def test_success_comment_image_upload(self, mocked_requests):
+        client = Client()
+
+        headers = {"HTTP_Authorization": self.token}
+        content_image = SimpleUploadedFile('요거.png', b'')       
+       
+        class MockedResponse:
+            def upload_fileobj(Fileobj, Bucket, Key, ExtraArgs):
+                return True
+
+            access_token  = headers["HTTP_Authorization"]
+            payload       = jwt.decode(access_token, settings.SECRET_KEY, algorithms = settings.ALGORITHM)
+            user          = User.objects.get(id = payload["id"])
+            content_image = 'fd.png'
+
+            file = upload_fileobj(
+                Fileobj='fd.png',
+                Bucket='minjeong',
+                Key="content_image/" + str(user.id) + "/" + str(content_image),
+                ExtraArgs={'ACL':'public-read'}
+            )
+
+        mocked_requests.return_value = MockedResponse()
+        response         = client.post('/contents/media', {'content_image' : content_image},**headers)
+        self.assertEqual(response.status_code, 201)
+        # self.assertEqual(response,
+        #     {
+        #         'message' : "https://minjeong.s3.ap-northeast-2.amazonaws.com/content_image/1/"+ str(content_image)
+        #     }
+        # )
+        
--- contents/urls.py
+++ contents/urls.py
@@ -5,4 +5,5 @@ from .views      import CommentUploadView, PostUploadView
 urlpatterns = [
     path("/<int:post_id>/comment", CommentUploadView.as_view()),
     path("/", PostUploadView.as_view()),
+    path("/media", ContentImageUploadView.as_view()), 
 ]
