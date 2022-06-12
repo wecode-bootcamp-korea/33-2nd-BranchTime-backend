5c397d8396e9805cf31021c7fc7ca97762ef61b2
minjeong kim <ebsgreat7@gmail.com>
2022-06-13 07:09:31 +0900

    [FEAT] 댓글 입력 기능 추가
    1.core앱
    -글쓰기 내 이미지 업로드 기능 커밋에서 관련 내용을 추가했기 때문에 생략
    
    2.content앱
    -commentupload view에는 png, jpg, gif, jpeg 확장자 말고는 들어 올 수 없게 예외처리 -> 이미지 업로드 공간이고 댓글에 이미지 파일만 가능하기 때문에 예외처리
    
    -key는 'comment_image' + post_id + image 로 구성 -> 글 별로 댓글이 있기 때문에 작성된 글을 기준으로 관리

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
+import os
 
-# Create your views here.
+from django.views    import View
+from django.http     import JsonResponse
+
+import boto3
+import logging
+from my_settings         import AWS_STORAGE_BUCKET_NAME, AWS_SECRET_ACCESS_KEY
+from botocore.exceptions import ClientError
+
+bucket = AWS_STORAGE_BUCKET_NAME
+client = boto3.client('s3')
+
+def upload_fileobj(Fileobj, Bucket, Key, ExtraArgs, Callback=None, Config=None):
+    try:
+        client.upload_fileobj(Fileobj, Bucket, Key, ExtraArgs)
+
+    except ClientError as e:
+        logging.error(e)
+        return False
+
+    return True
