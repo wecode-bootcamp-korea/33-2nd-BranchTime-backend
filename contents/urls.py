65932b3c0eb683e9c323fd1ea3c6ea255bbb4289
minjeong kim <ebsgreat7@gmail.com>
2022-06-13 07:09:31 +0900

    [FEAT] 댓글 입력 기능 추가
    1.core앱
    -글쓰기 내 이미지 업로드 기능 커밋에서 관련 내용을 추가했기 때문에 생략
    
    2.content앱
    -commentupload view에는 png, jpg, gif, jpeg 확장자 말고는 들어 올 수 없게 예외처리 -> 이미지 업로드 공간이고 댓글에 이미지 파일만 가능하기 때문에 예외처리
    
    -key는 'comment_image' + post_id + image 로 구성 -> 글 별로 댓글이 있기 때문에 작성된 글을 기준으로 관리

--- contents/urls.py
+++ contents/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from .views import CommentUploadView
+
 urlpatterns = [
+    path("/<int:post_id>/comment", CommentUploadView.as_view()),
 ]
--- contents/views.py
+++ contents/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+import json
+import datetime
 
-# Create your views here.
+from django.views    import View
+from django.http     import JsonResponse
+
+from    core.views          import upload_fileobj
+from    my_settings         import MEDIA_URL, AWS_STORAGE_BUCKET_NAME
+from    core.utils          import login_decorator
+from    contents.models     import Comment
+
+bucket = AWS_STORAGE_BUCKET_NAME
+args   = {'ACL':'public-read'}
+
+class CommentUploadView(View):
+    @login_decorator
+    def post(self, request, post_id):
+        try:
+            content         = request.POST["content"]
+            user            = request.user
+            image           = request.FILES['comment_image']
+
+            if not str(image).split('.')[-1] in ['png', 'jpg', 'gif', 'jpeg']:
+                return JsonResponse({"message" : "INVALID EXTENSION"}, status=400)
+
+            key = "comment_image/" + str(post_id) + "/" + str(image) 
+
+            upload_fileobj(Fileobj=image, Bucket=bucket, Key=key, ExtraArgs=args)
+
+            image_url = MEDIA_URL + key
+
+            Comment.objects.create(
+                content = content,
+                user_id = user.id,
+                image   = image_url,
+                post_id = post_id
+            )
+
+            return JsonResponse({'message' : "SUCCESS"}, status=201)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
