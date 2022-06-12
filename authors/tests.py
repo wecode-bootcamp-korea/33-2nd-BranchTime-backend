976fa2f8117a9b3a1425528449ed992410d7226b
minjeong kim <ebsgreat7@gmail.com>
2022-06-12 16:18:45 +0900

    [FEAT] 이메일 제안서 발송 기능
    -credentials.json -> 구글 클라우드 플랫폼에서 관련 토큰을 받기 위한 정보를 담은 파일 다운로드
    
    -token.json -> credentials.json을 통해 발급
    
    -credentials.json, token.json은 .gitignore에 추가 -> 보안상 관리
    
    -app.py -> 제안서는 ebsgreat7@gmail.com-> branchtime 관리자가 보내지만 제안하는 사람의 이메일을 제안서에 첨부해야 하기 때문에 content에 첨부해서 이메일 보내는 로직 구성,
    GoogleEmail 클래스 생성 및 generate_token, sendmail 함수 구성 -> 구글이메일api(외부api)사용으로 분리
    
    -authors앱 views.py ->ProposalView 생성 및 제안관련 내용 db 저장하는 로직 구성

--- authors/tests.py
+++ authors/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+import json
+import bcrypt
+import jwt
 
-# Create your tests here.
+from django.test import TestCase, Client
+from json        import dumps
+from django.conf import settings
+
+from .models  import Author, User, ProposalObject, Proposal
+from unittest import mock
+from unittest.mock import patch, MagicMock
+
+class EmailTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id           = 1,
+            email        = "dno06103@naver.com",
+            name         = "김민정",
+            thumbnail    = "저거.png",
+            introduction = "블라블라블라블라",
+        )
+
+        Author.objects.create(
+            id           = 1,
+            introduction = "블라블라",
+            career       = "블라블라블라블라",
+            user_id      = 1
+        )
+
+        ProposalObject.objects.create(
+            id = 1,
+            name = "출판기고",
+        )
+        
+        self.token = jwt.encode({'id':User.objects.get(id=1).id}, settings.SECRET_KEY, settings.ALGORITHM)
+
+    def tearDown(self):
+        Author.objects.all().delete()
+        User.objects.all().delete()
+        Proposal.objects.all().delete()
+
+    @patch("authors.app.GoogleEmail")
+    def test_success_proposalview_post(self, mocked_requests):
+        client = Client()
+        data = {
+            "content"        : "블라블라",
+            "sender_email"   : "dno06101@naver.com",
+            "proposal_object": "출판기고"
+        }
+    
+        headers = {"HTTP_Authorization": self.token}
+
+        class MockedResponse:
+            # def __init__(self):
+            #     credentials = credentials
+
+            def generate_token(self, file_path):
+                return "<google.oauth2.credentials.Credentials object at 0x111a4ff10>"
+
+            file_path = 'token.json'
+
+            def send_email(self, content, author, sender_email, creds):
+                return JsonResponse({"message" : "SUCCESS"}, status=200)            
+
+            content      = "블라블라"
+            sender_email = "dno06103@naver.com"
+            author       = Author.objects.get(id=1)
+            creds        = generate_token(self, file_path)
+
+        access_token = headers["HTTP_Authorization"]
+        payload      = jwt.decode(access_token, settings.SECRET_KEY, algorithms = settings.ALGORITHM)
+        user         = User.objects.get(id = payload["id"])
+
+        Proposal.objects.create(
+            sender_email       = "dno06103@naver.com",
+            content            = "블라블라",
+            proposal_object_id = 1,
+            author_id          = 1,
+            user_id            = user.id
+        )
+        mocked_requests.return_value = MockedResponse()
+
+        response = client.post('/authors/1', json.dumps(data), content_type='application/json', **headers)
+
+        self.assertEqual(response.json(), {'message' : 'SUCCESS'})
+        self.assertEqual(response.status_code, 201)
--- authors/urls.py
+++ authors/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from authors.views import ProposalView
+
 urlpatterns = [
+    path("/<int:author_id>", ProposalView.as_view()),
 ]
