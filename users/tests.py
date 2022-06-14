d337a2654731a78c700879414aceebb8e79e0e00
sorkihg <sorkihg@gmail.com>
2022-06-14 15:46:04 +0900

    [BranchTime팀] ProfileUpdate 구현

--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,12 @@
-import jwt
+import jwt, json
 
 from django.conf import settings
 from django.test import TestCase, Client
 from unittest.mock import MagicMock, patch
+from django.db import transaction
 
-from users.models import User
-
+from users.models import SocialAccount, Subscription, User
+from authors.models import Author, InterestedAuthor
 
 @patch('users.views.requests')
 class KakaoLoginViewTest(TestCase):
@@ -41,10 +42,11 @@ class KakaoLoginViewTest(TestCase):
                             },
                         }
                     
+
         mocked_requests.get = MagicMock(return_value = MockedResponse())
 
         headers  = {"HTTP_Authoriazation": "kakao_token"}
-        response = client.get("/users/kakao/callback", content_type = 'application/json', **headers)
+        response = client.get("/users/kakao", content_type = 'application/json', **headers)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), 
             {
@@ -53,8 +55,94 @@ class KakaoLoginViewTest(TestCase):
                     )
             }
         )
-        
+
         token   = response.json()['token']
         user_id = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)['id']
         user    = User.objects.get(id=user_id)
         self.assertEqual(user.email, "abcd@naver.com")
+
+
+class UserDetailViewTest(TestCase):
+    def setUp(self):
+        
+        with transaction.atomic():
+            User.objects.create(
+                id = 1,
+                name         = "홍길동",
+                email        = "test@gmail.com",
+                thumbnail    = "test.jpg",
+                introduction = "홍길동님의 BranchTime입니다."
+            )
+            SocialAccount.objects.create(
+                        id = 1,
+                        social_account_id = "123123123",
+                        name              = "kakao",
+                        user_id           = 1
+                        )
+        
+        Author.objects.create(
+            id = 1,
+            introduction = "나는 작가 홍길동입니다",
+            career = "한국대학교 졸업",
+            user_id = 1
+        )
+
+        with transaction.atomic():
+            User.objects.create(
+                id = 2,
+                name         = "김길동",
+                email        = "test1@gmail.com",
+                thumbnail    = "test1.jpg",
+                introduction = "김길동님의 BranchTime입니다."
+            )
+            SocialAccount.objects.create(
+                        id = 2,
+                        social_account_id = "123123456",
+                        name              = "kakao",
+                        user_id           = 2
+                        )
+
+        self.token = jwt.encode({'id':User.objects.get(id=1).id}, settings.SECRET_KEY, settings.ALGORITHM)
+
+    def tearDown(self):
+        User.objects.all().delete()  
+        Author.objects.all().delete()  
+
+    def test_user_detail_view_success(self):
+        client = Client()        
+
+        headers = {"HTTP_Authorization":self.token}
+        
+        response = client.get('/users/mypage', content_type='application/json', **headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "user_detail": {
+                "id": 1, 
+                "name": "홍길동", 
+                "description": "나는 작가 홍길동입니다", 
+                "avatar": "test.jpg",
+                "subscriber": 0,
+                "interestedAuthor": 0,
+                "career": "한국대학교 졸업"
+                }
+            }
+        )
+
+    def test_user_detail_view_invalid_token(self):
+        client = Client()        
+        
+        headers = {"HTTP_Authorization":"가짜_token"}
+        
+        response = client.get('/users/mypage', content_type='application/json', **headers)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message" : "INVALID TOKEN"})    
+
+
+    def test_user_detail_view_invalid_user(self):
+        client = Client()        
+        self.token = jwt.encode({'id':3}, settings.SECRET_KEY, settings.ALGORITHM)
+        headers = {"HTTP_Authorization":self.token}
+        
+        response = client.get('/users/mypage', content_type='application/json', **headers)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message" : "INVAILD_USER"})        
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
+from xml.etree.ElementInclude import include
 from django.urls import path
 
-from .views import KakaoLoginView
+from .views import KakaoLoginView, UserDetailView
 
 urlpatterns = [
     path('/kakao', KakaoLoginView.as_view()),
+    path('/mypage', UserDetailView.as_view()),
 ]
