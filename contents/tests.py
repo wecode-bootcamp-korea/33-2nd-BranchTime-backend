c865d0ae1eb1b220e12ecb8d03effa8cb171e025
sorkihg <sorkihg@gmail.com>
2022-06-15 22:04:20 +0900

    [BranchTime팀] PostSubListView 구현
    [구현사항]
    1. post에 subcategory의 경로로 별도의 페이지가 있어 API 추가구현
    2. 기능자체는 PostListView 와 동일하지만 PostListView는 maincategory에서 시작하지만 PostSubListView는 subcategoty로부터 시작한다는 점이 다른점입니다.

--- contents/tests.py
+++ contents/tests.py
@@ -5,10 +5,13 @@ from io import BytesIO
 
 from django.test       import TestCase, Client
 from django.conf       import settings
+from django.db import transaction
 
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from .models           import User
+from users.models import SocialAccount
+
+from .models           import MainCategory, Post, SubCategory, User
 from unittest          import mock
 from unittest.mock     import patch, MagicMock
 from core.views        import upload_fileobj
@@ -61,3 +64,149 @@ class CommentImageUploadTest(TestCase):
         #     }
         # )
         
+
+
+
+class PostListViewTest(TestCase):
+    def setUp(self):
+        with transaction.atomic():
+            User.objects.create(
+                id = 1,
+                name         = "홍길동",
+                email        = "test@gmail.com",
+                thumbnail    = "test.jpg",
+                introduction = "홍길동님의 BranchTime입니다."
+            )
+            SocialAccount.objects.create(
+                        id = 1,
+                        social_account_id = "123123123",
+                        name              = "kakao",
+                        user_id           = 1
+                        )
+        
+        MainCategory.objects.create(
+            id           = 1,
+            name         = "개발 프로그래밍",
+        )
+        MainCategory.objects.create(
+            id           = 2,
+            name         = "웹 개발",
+        )
+        MainCategory.objects.create(
+            id           = 3,
+            name         = "프론트엔드",
+        )
+        MainCategory.objects.create(
+            id           = 4,
+            name         = "백엔드",
+        )
+        SubCategory.objects.create(
+            id              = 1,
+            name            = "협업",
+            maincategory_id = 1,
+        )
+        SubCategory.objects.create(
+            id              = 2,
+            name            = "메타버스",
+            maincategory_id = 1,
+        )
+        SubCategory.objects.create(
+            id              = 3,
+            name            = "소프트웨어 테스트",
+            maincategory_id = 1,
+        )
+        Post.objects.create(
+            title           = "1번글 제목입니다",
+            sub_title       = "1번 소제목입니다",
+            thumbnail_image = "test.jpg",
+            content         = "1번글 내용",
+            reading_time    = "13:10",
+            subcategory_id  = 1,
+            user_id         = 1
+            )
+        Post.objects.create(
+            title           = "2번글 제목입니다",
+            sub_title       = "2번 소제목입니다",
+            thumbnail_image = "test.jpg",
+            content         = "2번글 내용",
+            reading_time    = "13:10",
+            subcategory_id  = 2,
+            user_id         = 1
+            )
+        Post.objects.create(
+            title           = "3번글 제목입니다",
+            sub_title       = "3번 소제목입니다",
+            thumbnail_image = "test.jpg",
+            content         = "3번글 내용",
+            reading_time    = "13:10",
+            subcategory_id  = 3,
+            user_id         = 1
+            )
+
+    def tearDown(self):
+        MainCategory.objects.all().delete()  
+        SubCategory.objects.all().delete()  
+        Post.objects.all().delete()  
+
+    def test_post_list_view_success(self):
+        client = Client()        
+        print(client)
+        response = client.get('/contents/postlist/1', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),{
+            "title_list": {
+                "title"    : "개발 프로그래밍",
+                "sub_title": [
+                    {
+                        "id": 1,
+                        "name": "협업"
+                    },
+                    {
+                        "id": 2,
+                        "name": "메타버스"
+                    },
+                    {
+                        "id": 3,
+                        "name": "소프트웨어 테스트"
+                    }
+                ]
+            },
+            "post_list": [
+            [{
+                "id": 1,
+                "title": "1번글 제목입니다",
+                "subTitle": "1번 소제목입니다",
+                "desc": "1번글 내용",
+                "commentCount": 0,
+                "writeTime": "13:10:00",
+                "writeUser": "홍길동",
+                "imgSrc": "test.jpg"
+            }],
+            [{
+                "id": 2,
+                "title": "2번글 제목입니다",
+                "subTitle": "2번 소제목입니다",
+                "desc": "2번글 내용",
+                "commentCount": 0,
+                "writeTime": "13:10:00",
+                "writeUser": "홍길동",
+                "imgSrc": "test.jpg"
+            }],
+            [{
+                "id": 3,
+                "title": "3번글 제목입니다",
+                "subTitle": "3번 소제목입니다",
+                "desc": "3번글 내용",
+                "commentCount": 0,
+                "writeTime": "13:10:00",
+                "writeUser": "홍길동",
+                "imgSrc": "test.jpg"
+            }],]})
+            
+
+    def test_user_detail_view_doesnotexist(self):
+        client = Client()        
+        
+        response = client.get('/contents/postlist/5', content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"message":"DoesNotExist"})    
