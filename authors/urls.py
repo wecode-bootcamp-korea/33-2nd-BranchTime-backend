bdb86c15e35be87b1de2355c5495c10d9fe688d9
minjeong kim <ebsgreat7@gmail.com>
2022-06-15 16:27:24 +0900

    [FEAT] 작가 리스트 추출 기능

--- authors/urls.py
+++ authors/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from authors.views import ProposalView
+from authors.views import ProposalView, AuthorListView
 
 urlpatterns = [
     path("/<int:author_id>", ProposalView.as_view()),
+    path("/", AuthorListView.as_view())
 ]
