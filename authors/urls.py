9bdbf1ef47c98be1b26355c4d4b0e2924ef35cfa
minjeong kim <ebsgreat7@gmail.com>
2022-06-18 23:35:07 +0900

    [REFACTOR] Authors앱 리팩토링
    utils앱
    GoogleEmail 클래스 코드 정리
    
    Authors앱
    test.py 내 ProposalTest, AuthorListTest, AuthorDetailTest 클래스 작성
    urls.py 내 url 정리 -> Restful API 형식으로 url 정리
    views.py 내 AuthorDetailView 클래스 내 select_related 활용해 코드 최적화

--- authors/urls.py
+++ authors/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from authors.views import ProposalView, AuthorListView, AuthorDetailView
 
 urlpatterns = [
-    path("/<int:author_id>", ProposalView.as_view()),
-    path("/", AuthorListView.as_view()),
-    path("/detail/<int:author_id>", AuthorDetailView.as_view()),
+    path("/<int:author_id>/proposal", ProposalView.as_view()),
+    path("", AuthorListView.as_view()),
+    path("/<int:author_id>", AuthorDetailView.as_view()),
 ]
--- authors/views.py
+++ authors/views.py
@@ -8,69 +8,64 @@ from core.utils                     import login_decorator
 from authors.models                 import Author, Proposal, ProposalObject
 from contents.models                import SubCategory
 from google.oauth2.credentials      import Credentials
-from authors.app                    import GoogleEmail
+from utils.google_email_api         import GoogleEmail
 
 class ProposalView(View):
     @login_decorator
     def post(self, request, author_id):
         try:
-            data            = json.loads(request.body)
-            content         = data["content"]
-            sender_email    = data["sender_email"]
-            proposal_object = data["proposal_object"]
-            author          = Author.objects.get(id=author_id)
+            data               = json.loads(request.body)
+            content            = data["content"]
+            sender_email       = data["sender_email"]
+            proposal_object_id = data["proposal_object_id"]
+            author             = Author.objects.get(id=author_id)
 
-            google_email = GoogleEmail(credentials="credentials.json")
-            creds        = google_email.generate_token(file_path="token.json")
-            google_email.send_email(content = content, author = author, sender_email = sender_email, creds=creds)
-           
+            google_email = GoogleEmail()
+            google_email.generate_token(token_file_path="token.json", credentials_file_path="credentials.json")
+            google_email.send_email(content = content, author = author, sender_email = sender_email)
+
             Proposal.objects.create(
                 sender_email       = sender_email,
                 content            = content,
-                proposal_object_id = ProposalObject.objects.get(name=proposal_object).id,
+                proposal_object_id = ProposalObject.objects.get(id=proposal_object_id).id,
                 author_id          = author.id,
                 user_id            = request.user.id
             )
-        except HttpError as error:
-            send_message = None
-
-        return JsonResponse({"message" : "SUCCESS"}, status=201)
+            
+            return JsonResponse({"message" : "SUCCESS"}, status=201)
+        
+        except ProposalObject.DoesNotExist:
+            return JsonResponse({"message" : "NO_PROPOSAL_OBJECT"}, status=400)
 
 class AuthorListView(View):
     def get(self, request):
-        try:
-            
-            subcategory_id = request.GET.get("subcategory_id")
-            subcategory    = SubCategory.objects.get(id = subcategory_id)
-            authors        = Author.objects.select_related('user').filter(subcategory_id = subcategory.id)
-            
-            result = [{
-                "author_thumbnail"   : author.user.thumbnail,
-                'author_name'        : author.user.name,
-                "author_introduction": author.introduction,
-                "author_subcategory" : author.subcategory.name,
-                "author_id"          : author.id
-            }for author in authors]
+        
+        subcategory_id = request.GET.get("subcategory_id")
+        authors        = Author.objects.select_related('user').filter(subcategory_id = subcategory_id)
+        
+        result = [{
+            "author_thumbnail"   : author.user.thumbnail,
+            'author_name'        : author.user.name,
+            "author_introduction": author.introduction,
+            "author_subcategory" : author.subcategory.name,
+            "author_id"          : author.id
+        }for author in authors]
 
-            return JsonResponse({"message" : result}, status=201)
-
-        except KeyError :
-            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+        return JsonResponse({"message" : result}, status=201)
 
 class AuthorDetailView(View):
     def get(self, request, author_id):
         try:
-            authors = Author.objects.get(id = author_id)
+            authors = Author.objects.select_related('user').get(id = author_id)
             author_detail = {
                         "id"              : authors.id,
                         "name"            : authors.user.name,
                         "description"     : authors.introduction,
                         "avatar"          : authors.user.thumbnail,
-                        "subscriber"      : authors.user.subscription.all().count(),
-                        "interestedAuthor": authors.user.interestedauthor_set.all().count(),
+                        "subscriber"      : authors.user.subscription.count(),
+                        "interestedAuthor": authors.user.interestedauthor_set.count(),
                         }
-
             return JsonResponse({"author_detail" : author_detail}, status = 200)
 
         except Author.DoesNotExist:
-            return JsonResponse({"message":"DoesNotExist"}, status = 401)  
+            return JsonResponse({"message":"AUTHOR_NOT_EXIST"}, status = 401)  
